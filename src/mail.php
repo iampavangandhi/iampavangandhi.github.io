22698b1df994811691a9b030944fb3584e2db4c9
Pavan Rajesh Gandhi <42767012+iampavangandhi@users.noreply.github.com>
2019-06-06 10:28:17 +0530

    Add files via upload
    mail.php

---
+++ src/mail.php
@@ -0,0 +1,12 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$formcontent = "From: $name \n Message: $message";
+	$recipient = "pavangandhi100@gmail.com";
+	$subject = "Contact Form";
+	$mailheader = "From: $email \r\n";
+	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!")
+	echo "Thank You";
+?>
