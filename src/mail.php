65b9dbd4fe60f9ddef2d50d02963a598a5d044a9
Pavan Rajesh Gandhi <42767012+iampavangandhi@users.noreply.github.com>
2019-06-06 00:13:19 -0700

    Update mail.php

--- src/mail.php
+++ src/mail.php
@@ -3,10 +3,11 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 
+	$email_from = "pavangandhi171@gmail.com";
 	$formcontent = "From: $name \n Message: $message";
 	$recipient = "pavangandhi100@gmail.com";
 	$subject = "Contact Form";
-	$mailheader = "From: $email \r\n";
-	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-	echo "Thank You";
+	$mailheader = "From: $email_from \r\n"
+	$mailheader .= "Reply to: $email \r\n";
+	mail($recipient, $subject, $formcontent, $mailheader);
 ?>
